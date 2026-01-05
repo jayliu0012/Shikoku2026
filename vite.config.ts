e35ef2abd2b1cd1a6833717dfb9b24d1cedf6d9d
jayliu0012 <toomy1023@yahoo.com.tw>
2026-01-05 11:55:36 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  // 必須與您的 GitHub 儲存庫名稱完全一致
+  base: '/Shikoku2026/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    // 關閉 source map 以加快編譯並減少體積
+    sourcemap: false,
   }
 });
+
