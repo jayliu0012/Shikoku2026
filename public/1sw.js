5d6adac65575c4e2cfbb2f6a483c0b1c54efcf3a
jayliu0012 <toomy1023@yahoo.com.tw>
2026-01-05 13:35:11 +0800

    Add files via upload

---
+++ public/1sw.js
@@ -0,0 +1,36 @@
+
+const CACHE_NAME = 'shikoku-v4';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    Promise.all([
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+        );
+      }),
+      self.clients.claim() // 立即接管所有頁面
+    ])
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
